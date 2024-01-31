5b5164eeea0f6746a1187fec96fa90f21df7a919
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-01-30 16:26:38 -0800

    Remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import Link from "next/link";
 import HomepageLink from "@/component/homepageLink";
 
 const inter = Inter({ subsets: ["latin"] });
