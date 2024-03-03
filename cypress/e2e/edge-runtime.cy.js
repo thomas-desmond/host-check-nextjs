86aa3aaa3af61c0a74dea6a74fe1b671c3539507
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-03-03 15:39:12 -0800

    Page link fixes

--- cypress/e2e/edge-runtime.cy.js
+++ cypress/e2e/edge-runtime.cy.js
@@ -2,7 +2,7 @@ describe('Edge Runtime Support', () => {
     it('should fetch using Edge runtime and show Magikarp', () => {
       cy.visit(Cypress.env('url'))
    
-      cy.get('#edge-runtime').click()
+      cy.get('#edge-function').click()
       cy.url().should('include', '/edge-function')
       cy.get('p').contains('magikarp')
     })
--- cypress/e2e/jss-image-optimization.cy.js
+++ cypress/e2e/jss-image-optimization.cy.js
@@ -2,7 +2,7 @@ describe('Sitecore JS Image Optimization with NextImage Component', () => {
     it('should optimize images that use NextImage component', () => {
         cy.visit(Cypress.env('url'));
 
-        cy.get('#jss-image').click()
+        cy.get('#jss-image-optimization').click()
         cy.get('#original').invoke('text').as('originalSize')
         cy.get('#optimized').should('not.have.value', '0');
         cy.get('#optimized').invoke('text').as('optimizedSize')
