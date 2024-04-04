789daa67d2d1702911d98a814cdab0b34fd4950f
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-04-03 18:59:22 -0700

    Fix revalidate url

--- pages/api/revalidate-isr.ts
+++ pages/api/revalidate-isr.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        await res.revalidate('/api/revalidate-isr');
+        await res.revalidate('/feature/on-demand-isr');
         return res.json({ revalidated: true });
 
     } catch (err) {
