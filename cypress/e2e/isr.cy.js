0de0c336013aaec842598c521e4682b2a48ac685
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-07-15 07:31:58 -0700

    ISR test change

--- cypress/e2e/isr.cy.js
+++ cypress/e2e/isr.cy.js
@@ -7,10 +7,12 @@ describe('Incremental Static Regeneration Support', () => {
         let beforeISR = cy.get('p').invoke('text').as('beforeISR');
         cy.get('@beforeISR').then((beforeISR) => {
             const beforeFirstSlash = beforeISR.split('/')[0];
+            cy.reload();
             cy.wait(62000);
             cy.reload();
             cy.get('p').invoke('text').should('equal', beforeFirstSlash);
-            cy.wait(2000);
+            cy.wait(3000);
+            cy.reload();
             cy.reload();
             cy.get('p').invoke('text').should('not.equal', beforeFirstSlash);
         })
